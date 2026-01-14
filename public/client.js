46cbc5c9f67349bca65f1346976e60f0c0a2e73c
PiciMadar <rekdavid@turr.hu>
2026-01-14 10:34:29 +0100

    Funkcionalitás elkezdése, több kevesebb sikerrel

--- public/client.js
+++ public/client.js
@@ -1,2 +1,9 @@
 const socket = io();
 
+const jatekGombok = document.getElementsByClassName("JatekGombok")
+
+for(let i = 0; i < jatekGombok.length; i++){
+    jatekGombok[i].addEventListener('click',()=>{
+        alert(i)
+    })
+}
