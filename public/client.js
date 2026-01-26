71c5875d20af74a1e36a9e81f5544c358f1d1908
Zalan12 <farkaszalan@turr.hu>
2026-01-26 11:29:42 +0100

    Lépések logolása

--- public/client.js
+++ public/client.js
@@ -1,7 +1,8 @@
 const socket = io();
 
 const JatekGombok = document.getElementsByClassName("JatekGombok");
-const currentPlayer = 0;
+let currentPlayer = 0;
+let symbols=["X","0"];
 const config=window.CHAT_CONFIG;
 
 let map = [[0,0,0],[0,0,0],[0,0,0]];
@@ -15,17 +16,31 @@ for(let i = 0; i < JatekGombok.length; i++){
 
 socket.emit('join-room',{nickname:config.nickname,room:config.roomId})
 
+
 socket.on('GombAktival',({idx,player}) =>{
     console.log("INDEX: " + idx)
     console.log("JÁTÉKOS: " + config.nickname)
+
+
     JatekGombok[idx].classList.add("Tiltott")
     JatekGombok[idx].classList.remove("hoverEffekt")
 
     let kep = document.createElement('img');
-    kep.src = "assets/imgs/kekKor2.png"
+    if(currentPlayer==0)
+    {
+        kep.src = "assets/imgs/kekKor2.png";
+        currentPlayer++;
+    }
+    else{
+        kep.src = "assets/imgs/pirosX2.png";
+        currentPlayer--;
+    }
     kep.style.width = "80%";
     kep.style.height = "80%";
 
+    let log=document.getElementById('szovegfal');
+    log.innerText+=config.nickname+" megjelölte az " +idx+" indexű elemet "+ symbols[currentPlayer] +" szimbólummal\n";
+    console.log(map)
     JatekGombok[idx].appendChild(kep)
     JatekGombok[idx].disabled = "true"
 })
